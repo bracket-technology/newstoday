255bb249b1ef3f362eb3ab0f625a10b6a722b9bc
sangkanfaiq <sangkanfaiq@gmail.com>
2022-07-23 22:22:44 +0700

    feat: add profile page [not fix]

--- src/Pages/ProfilePage/ProfilePage.js
+++ src/Pages/ProfilePage/ProfilePage.js
@@ -1,22 +1,67 @@
 import React from "react";
-import './styles.scss'
-import './mobile.scss'
-import NavbarUser from "../../Components/Navbar/NavbarUser";
-import avatar from '../../assets/users/user2.jpg'
-import verif from '../../assets/icons/checked.svg'
+import "./styles.scss";
+import "./mobile.scss";
+import Navbar from "../../Components/Navbar";
+import avatar from "../../assets/users/user2.jpg";
+import verif from "../../assets/icons/checked.svg";
 
 const ProfilePage = () => {
   return (
     <>
-      <NavbarUser />
+      <Navbar />
       <div className="container">
         <div className="profile-page">
-          <div className="user-box">
-            <img src={avatar} alt="user" />
-            <div className="profile-details">
-              <h4>@kendalljenner</h4>
-              <h2>Kendall Jenner</h2>
-              <p>Author<span><img src={verif} alt="verified" /></span></p>
+          <div className="row">
+            <div className="user-box1 col-md-4">
+              <h2>Profile</h2>
+              <div className="user-box1-card">
+                <div className="user-box1-card-img">
+                  <img src={avatar} alt="avatar" />
+                </div>
+                <input type="file" />
+              </div>
+            </div>
+            <div className="user-box2 col-md-6">
+              <div className="user-box2-img">
+                <img src={avatar} alt="user" />
+              </div>
+              <div className="profile-details">
+                <h4>@madison</h4>
+                <h2>Madison</h2>
+                <p>
+                  Author
+                  <span>
+                    <img src={verif} alt="verified" />
+                  </span>
+                </p>
+                <div className="about-me">
+                  <h3>About me</h3>
+                  <p>
+                    Madison Blackstone is a director of publisher, with
+                    experience managing global teams.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-2"></div>
+          </div>
+          <div className="row">
+            <div className="col-md-4"></div>
+            <div className="col-md-6">
+              <div className="author-panel col-md-6">
+                <div className="panel-card">
+                  <p>16</p>
+                  <p>POST</p>
+                </div>
+                <div className="panel-card">
+                  <p>16.5k</p>
+                  <p>LIKE</p>
+                </div>
+                <div className="panel-card">
+                  <p>160</p>
+                  <p>COMMENT</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
