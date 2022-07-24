374b825176735d4247c21e2d5621f29366fa6aa6
sangkanfaiq <sangkanfaiq@gmail.com>
2022-07-24 16:13:28 +0700

    feat: add navbar dark mode

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,34 +1,80 @@
 import React, { useState } from "react";
 import "./styles.scss";
 import { FaBars, FaTimes } from "react-icons/fa";
-import './mobile.scss'
-import { Link } from 'react-router-dom'
+import "./mobile.scss";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({isDark}) => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="navbar">
-      <div className="container">
-        <div className="logo">
-          <h2>
-            News <span>Today</span>
-          </h2>
-        </div>
-        <div className={toggle ? "nav-menu active" : "nav-menu"}>
-          <Link to="/"><div className="nav-links active">Home</div></Link>
-          <div className="nav-links">Article</div>
-          <div className="nav-links">Category</div>
-          <div className="nav-links">About</div>
-          <div className="button">
-            <Link to="/signup"><button>Signup</button></Link>
-            <Link to="/signin"><button>Login</button></Link>
+    <>
+      {isDark ? (
+        <nav className="navbar-dark">
+          <div className="container nav">
+            <div className="logo">
+              <Link to="/">
+                <h2>
+                  News <span>Today</span>
+                </h2>
+              </Link>
+            </div>
+            <div className={toggle ? "nav-menu active" : "nav-menu"}>
+              <Link to="/">
+                <div className="nav-links active">Home</div>
+              </Link>
+              <div className="nav-links">Article</div>
+              <div className="nav-links">Category</div>
+              <div className="nav-links">About</div>
+              <div className="button">
+                <Link to="/signup">
+                  <button>Signup</button>
+                </Link>
+                <Link to="/signin">
+                  <button>Login</button>
+                </Link>
+              </div>
+              <p>Copyright© 2022 News Today. All right reserved.</p>
+            </div>
+            <div className="nav-icon" onClick={() => setToggle(!toggle)}>
+              {toggle ? <FaTimes /> : <FaBars />}
+            </div>
           </div>
-          <p>Copyright© 2022 News Today. All right reserved.</p>
-        </div>
-        <div className="nav-icon" onClick={() => setToggle(!toggle)}>{toggle ? <FaTimes /> : <FaBars />}</div>
-      </div>
-    </nav>
+        </nav>
+      ) : (
+        <nav className="navbar">
+          <div className="container">
+            <div className="logo">
+              <Link to="/">
+                <h2>
+                  News <span>Today</span>
+                </h2>
+              </Link>
+            </div>
+            <div className={toggle ? "nav-menu active" : "nav-menu"}>
+              <Link to="/">
+                <div className="nav-links active">Home</div>
+              </Link>
+              <div className="nav-links">Article</div>
+              <div className="nav-links">Category</div>
+              <div className="nav-links">About</div>
+              <div className="button">
+                <Link to="/signup">
+                  <button>Signup</button>
+                </Link>
+                <Link to="/signin">
+                  <button>Login</button>
+                </Link>
+              </div>
+              <p>Copyright© 2022 News Today. All right reserved.</p>
+            </div>
+            <div className="nav-icon" onClick={() => setToggle(!toggle)}>
+              {toggle ? <FaTimes /> : <FaBars />}
+            </div>
+          </div>
+        </nav>
+      )}
+    </>
   );
 };
 
