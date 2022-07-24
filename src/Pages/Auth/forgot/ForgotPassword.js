b92ac57353f7a8e3b39ef1e61b4116d0ea77ab6b
dhanisetiaji <dhanisetiaji99@gmail.com>
2022-07-24 23:52:53 +0700

    feat:add forgot pass with email

--- src/Pages/Auth/forgot/ForgotPassword.js
+++ src/Pages/Auth/forgot/ForgotPassword.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./styles.css";
 import "./mobile.css";
 import logo from "../../../assets/svg/bg3.svg";
+import { SendForgot } from "../../../Redux/actions/Verify";
+import Metatags from "../../../Components/Metatags";
 
 const ForgotPassword = () => {
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = React.useState({});
+  const { loading } = useSelector((s) => s.verify);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(SendForgot(email));
+  }
+
   return (
     <>
+      <Metatags title={'Forgot Password | News Today'} />
       <div className="box-fp">
         <div className="split-fp left-fp">
-          <img src={logo} alt="" />
+          <img src={logo} alt="logo" />
         </div>
 
         <div className="split-fp right-fp">
@@ -19,14 +34,18 @@ const ForgotPassword = () => {
               address that you used to register and we will give you
               instructions to reset your password.
             </h3>
-            <form>
+            <form onSubmit={(e) => handleSubmit(e)}>
               <label htmlFor="">Email</label>
               <br />
-              <input type="text" placeholder="Enter your email" />
+              <input type="email" placeholder="Enter your email" onChange={(e) => setEmail((prevData) => ({ ...prevData, email: e.target.value }))} />
               <br />
               <br />
               <div className="button-fp d-flex justify-content-end">
-                <button>Send</button>
+                {loading ? (
+                  <button type="submit" disabled> Loading... </button>
+                ) : (
+                  <button type="submit">Send</button>
+                )}
               </div>
               <br />
             </form>
