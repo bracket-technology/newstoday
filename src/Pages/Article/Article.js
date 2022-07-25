9812c5540880769df455f3c39c78c4cb38904e8c
sangkanfaiq <sangkanfaiq@gmail.com>
2022-07-26 00:49:15 +0700

    feat: fix article page

--- src/Pages/Article/Article.js
+++ src/Pages/Article/Article.js
@@ -2,28 +2,75 @@ import React from "react";
 import "./styles.scss";
 import "./mobile.scss";
 import Navbar from "../../Components/Navbar/Navbar";
-import Footer from '../../Components/Footer/Footer'
+import Footer from "../../Components/Footer/Footer";
 import h1 from "../../assets/news/k1.jpg";
 import h2 from "../../assets/news/n2.jpg";
 import h3 from "../../assets/news/n3.jpg";
 import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import { FiChevronsRight, FiSearch } from "react-icons/fi";
 
 const Article = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <>
       <Navbar />
       <div className="article-page">
-        <div className="article-header container">
-          <div className="header-content">
-            <img src={h1} alt="" />
-            {/* <div className="header1-content-info">
-              <h1>
-                Shinzo Abe’s Murder, Neighbors Reveal the Attitude of Shooters
-              </h1>
-            </div> */}
+        <div className="article-search container">
+          <div className="article-search-bar">
+            <input type="text" placeholder="Search..." />
+            <FiSearch className="search-icon" />
           </div>
-            
+          <div className="dropdown">
+            <button className="dropdown-toggle" data-bs-toggle="dropdown">
+              Order By
+            </button>
+            <ul class="dropdown-menu">
+              <li className="dropdown-item">ASC</li>
+              <li className="dropdown-item">DESC</li>
+            </ul>
+          </div>
+        </div>
+        <div className="article-header container">
+          <Slider {...settings}>
+            <div className="header-content">
+              <img src={h1} alt="news" />
+            </div>
+            <div className="header-content">
+              <img src={h2} alt="news" />
+            </div>
+          </Slider>
+
           <div className="article-card">
+            <div className="card-tag1">
+              <h4>Today's Popular</h4>
+              <div className="see-more">
+                <Link to="/#">See more</Link>
+                <FiChevronsRight className="see-more-icon" />
+              </div>
+            </div>
+            <div className="card-tag2">
+              <h4>Entertainment</h4>
+              <div className="see-more">
+                <Link to="/#">See more</Link>
+                <FiChevronsRight className="see-more-icon" />
+              </div>
+            </div>
+            <div className="card-tag3">
+              <h4>Sports</h4>
+              <div className="see-more">
+                <Link to="/#">See more</Link>
+                <FiChevronsRight className="see-more-icon" />
+              </div>
+            </div>
             <div className="card-layout-1">
               <div className="card-layout-1-box">
                 <img src={h2} alt="" />
@@ -77,8 +124,15 @@ const Article = () => {
                 <img src={h3} alt="" />
               </div>
               <div className="card-layout-5-content">
-                <Link to="/#"><h3>The Dangers Of Spyware Hermit Snoops That You Should Know</h3></Link>
-                <p>Advanced spyware campaigns are getting help from internet service providers.</p>
+                <Link to="/#">
+                  <h3>
+                    The Dangers Of Spyware Hermit Snoops That You Should Know
+                  </h3>
+                </Link>
+                <p>
+                  Advanced spyware campaigns are getting help from internet
+                  service providers.
+                </p>
                 <span>Healthy</span>
               </div>
             </div>
@@ -119,12 +173,19 @@ const Article = () => {
               </div>
             </div>
             <div className="card-layout-9">
-            <div className="card-layout-9-box">
+              <div className="card-layout-9-box">
                 <img src={h1} alt="" />
               </div>
               <div className="card-layout-9-content">
-                <Link to="/#"><h3>The Dangers Of Spyware Hermit Snoops That You Should Know</h3></Link>
-                <p>Advanced spyware campaigns are getting help from internet service providers.</p>
+                <Link to="/#">
+                  <h3>
+                    The Dangers Of Spyware Hermit Snoops That You Should Know
+                  </h3>
+                </Link>
+                <p>
+                  Advanced spyware campaigns are getting help from internet
+                  service providers.
+                </p>
                 <span>Healthy</span>
               </div>
             </div>
