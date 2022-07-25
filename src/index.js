ec081dda638555dbf9b72849a8c0439d3e4348e2
sangkanfaiq <sangkanfaiq@gmail.com>
2022-07-25 16:15:28 +0700

    feat: add search bar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import 'quill/dist/quill.snow.css'
